87973dbe6813c363a6f5218a7a856a9ccce2edb0
punknight <punknight16@gmail.com>
2019-02-24 11:33:57 -0800

    removed _config from gitignore

---
+++ _config/cred.js
@@ -0,0 +1,5 @@
+const pass_secret = 'so_secret';
+const token_secret = 'more_secret';
+
+exports.pass_secret = pass_secret;
+exports.token_secret = token_secret;
--- server.js
+++ server.js
@@ -304,7 +304,7 @@ var server = http.createServer(function(req, res){
 		default: 
 			res.end('bad request');
 	}
-}).listen(3002, function(){
+}).listen(process.env.PORT || 3002, function(){
 	console.log('server is up and running on 3002');
 })
 
